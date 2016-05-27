da3b7bfd52c499221c314e6250c9e2945efe918b
Janelle Nomad <jheron3@my.centennialcollege.ca>
2016-05-27 18:37:29 -0400

    Contains a practice solution of Lab assignment 2

---
+++ Lab2_practice_UnitTest/Lab2_practiceUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab2;
+
+namespace Lab2_practice_UnitTest
+{
+    [TestClass]
+    public class Lab2_practiceUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
